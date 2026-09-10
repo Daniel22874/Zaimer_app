722a47226c41fcb727f56d881bd40e4208816d54
Daniel_ATOMS <danilrodionov8665@gmail.com>
2026-09-10 22:30:47 +0300

    Технически реализовал главное окно

---
+++ main.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+from PyQt5 import uic
+import sys
+from test import len_rows, get_data, add_client
+
+
+class Main_window(QMainWindow):
+	def __init__(self):
+		super(Main_window, self).__init__()
+		uic.loadUi('main_window.ui', self)
+		self.setUI()
+
+	def setUI(self):
+		self.table = QTableWidget()
+		self.table.setRowCount(len_rows())
+		self.table.setColumnCount(4)
+		self.table.setHorizontalHeaderLabels(["id", "ФИО", "Паспорт", "Телефон"])
+
+		for row in range(len(get_data())):
+			for col in range(len(get_data()[row])):
+				self.table.setItem(row, col, QTableWidgetItem(get_data()[row][col]))
+
+		self.verticalLayout.addWidget(self.table)
+		self.pushButton.clicked.connect(self.in_dialog)
+
+	def in_dialog(self):
+		self.dialog = Dialog_window()
+		self.dialog.show()
+
+
+class Dialog_window(QDialog):
+	def __init__(self):
+		super(Dialog_window, self).__init__()
+		uic.loadUi("dialog_for_main.ui", self)
+		self.buttonBox.accepted.connect(lambda: add_client(self.line_fio.text(),
+				self.line_passport.text(), self.line_phone.text()))
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	win = Main_window()
+	win.show()
+	sys.exit(app.exec_())
---
+++ test.py
@@ -0,0 +1,23 @@
+import sqlite3 as sq
+
+
+def len_rows():
+    with sq.connect("zaimer.db") as connect:
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM clients")
+        res = cursor.fetchall()
+        return len(res)
+
+
+def get_data():
+    with sq.connect("zaimer.db") as connect:
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM clients")
+        res = cursor.fetchall()
+        return res
+
+
+def add_client(fio, passport, phone):
+    with sq.connect("zaimer.db") as connect:
+        cursor = connect.cursor()
+        cursor.execute("INSERT INTO clients (full_name, passport, phone) VALUES (?, ?, ?)", (fio, passport, phone))
